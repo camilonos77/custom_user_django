c7034bbb77d3a311ffd7596a5b4ffc79d4ffc273
root <camilonosdocs@gmail.com>
2015-11-03 12:08:40 -0500

    first commit

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/camilonos/Documentos/proyectos_2015/custom_user_django/custom_user/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
